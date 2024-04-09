dee4ae593c7b7904c540f26a84aef14523007260
sahil singh <sahilpython@gmail.com>
2024-04-09 18:23:12 +0530

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+    {
+      imgURL: '/icons/Home.svg',
+      route: '/',
+      label: 'Home',
+    },
+  
+    {
+      imgURL: '/icons/upcoming.svg',
+      route: '/upcoming',
+      label: 'Upcoming',
+    },
+    {
+      imgURL: '/icons/previous.svg',
+      route: '/previous',
+      label: 'Previous',
+    },
+    {
+      imgURL: '/icons/Video.svg',
+      route: '/recordings',
+      label: 'Recordings',
+    },
+    {
+      label: 'Personal Room',
+      route: '/personal-room',
+      imgURL: '/icons/add-personal.svg',
+    },
+  ];
+  
+  export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.png',
+    '/images/avatar-4.png',
+    '/images/avatar-5.png',
+  ];
